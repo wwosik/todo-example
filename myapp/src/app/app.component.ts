ec5d88196d0f44c14ca24be48dc171d776b0e10c
Wojciech Wosik <wojciech@wosik.pl>
2020-08-11 09:39:24 +0200

    Full client features

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  selectedTodo: Todo;
 
-  todos = [{
+  todos: Todo[] = [{
     text: 'Activity 1',
+    id: 1
   }, {
-    text: 'Activity 2'
+    text: 'Activity 2',
+    id: 2
   }]
+
+  todoText = new FormControl('', [Validators.required]);
+
+  save() {
+    if (this.selectedTodo) {
+      this.selectedTodo.text = this.todoText.value;
+      this.selectedTodo = null;
+    } else {
+      const todo: Todo = {
+        text: this.todoText.value,
+        id: new Date().valueOf()
+      }
+
+      this.todos.push(todo);
+
+    }
+    this.todoText.reset();
+  }
+
+  select(todo: Todo) {
+    this.selectedTodo = todo;
+    this.todoText.setValue(todo.text);
+  }
+
+  delete(todo: Todo) {
+    this.todos = this.todos.filter(t => t !== todo);
+  }
+}
+
+export interface Todo {
+  text: string
+  id: number
 }
